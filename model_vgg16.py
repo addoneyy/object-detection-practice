8b0218bef538da38b8123e88cb3a78a2ea525b7b
addoneyy <addone911@gmail.com>
2026-02-25 21:43:35 +0800

    update FCN

--- model_vgg16.py
+++ model_vgg16.py
@@ -11,12 +11,25 @@ class Testmodel(nn.Module):
     def __init__(self):
         super().__init__()
         self.feature_extract = vgg16().features
+        self.fc_layers = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(512*14*14, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 8) # 4 coordinate + 4 class
+        )
 
     def forward(self, x):
-        return self.feature_extract(x)
+        x = self.feature_extract(x)
+        return self.fc_layers(x)
 
 
 
 if __name__ == '__main__':
     model = Testmodel()
     print(model)
+    input = torch.rand(1, 3, 448, 448) #batch：数量
+    output = model(input)
+    print(output)
+    print(output.shape) #[1, 512, 14, 14]
